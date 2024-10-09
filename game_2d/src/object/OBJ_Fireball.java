5674774db45dd74bea94945810a53651061f9fc4
Tony Abi Fadel <tonyabifadel4@gmail.com>
2024-10-09 15:01:00 +0300

    Added fireball for player

---
+++ game_2d/src/object/OBJ_Fireball.java
@@ -0,0 +1,36 @@
+package object;
+
+import entity.Projectile;
+import game_2d.GamePanel;
+
+public class OBJ_Fireball extends Projectile{
+
+	GamePanel gp;
+	public OBJ_Fireball(GamePanel gp) {
+		super(gp);
+		this.gp = gp;
+		name  = "Fireball";
+		//speed is how fast the fireball is
+		speed = 5;
+		//maxlife is how many frames the fireball stay on screen
+		maxLife = 80;
+		life = maxLife;
+		attack = 2;
+		useCost = 1;
+		alive = false;
+		getImage();
+		
+	}
+	private void getImage() {
+		up1 = setup("/projectile/fireball_up_1",gp.tileSize,gp.tileSize); 
+		up2 = setup("/projectile/fireball_up_2",gp.tileSize,gp.tileSize); 
+		down1 = setup("/projectile/fireball_down_1",gp.tileSize,gp.tileSize); 
+		down2 = setup("/projectile/fireball_down_2",gp.tileSize,gp.tileSize); 
+		left1 = setup("/projectile/fireball_left_1",gp.tileSize,gp.tileSize); 
+		left2 = setup("/projectile/fireball_left_2",gp.tileSize,gp.tileSize); 
+		right1 = setup("/projectile/fireball_right_1",gp.tileSize,gp.tileSize); 
+		right2 = setup("/projectile/fireball_right_2",gp.tileSize,gp.tileSize); 
+		
+	}
+
+}
